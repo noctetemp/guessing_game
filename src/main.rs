96558fc2cdf43350b9e0a195a7a61957ba66e40e
noctetemp <h.fadhloun@gmail.com>
2023-03-27 22:27:47 +0900

    chapterII

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)  
+        .expect("Failed to read line");
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too Big!"),
+        Ordering::Greater => println!("Too Small!"),
+        Ordering::Equal => println!("YOU win!"),
+    }
+
+    println!("You guessed: {}", guess);
 }
